3cf2f04b8d9d54bd5f18033d61532f4a3d6fcbb2
jnhyk <101388279+jnhyk@users.noreply.github.com>
2022-05-11 12:52:42 +0900

    Update 2048_start.py
    시작버튼을 누르면 게임화면으로 전환

--- 2048_start.py
+++ 2048_start.py
@@ -1,18 +1,22 @@
 import pygame
 
+def display_start_screen():
+  global start_button
+  background = pygame.image.load("C:/Users/hth09/OneDrive/바탕 화면/desktop/2048/start_screen.jpg")
+  screen.blit(background, (0,0))
+  start_button = pygame.Rect(150, 550, 250 , 100)#종료 버튼
+
+def display_game_screen():
+  background = pygame.image.load("C:/Users/hth09/OneDrive/바탕 화면/desktop/2048/main.jpg")
+  screen.blit(background, (0,0))
 pygame.init()
 screen_width = 540
 screen_height = 725
 
 screen = pygame.display.set_mode((screen_width, screen_height))#배경
 pygame.display.set_caption("2048")
-background = pygame.image.load("C:/Users/hth09/OneDrive/바탕 화면/desktop/2048/start_screen.jpg")
-screen.blit(background, (0,0))
-
-start_button = pygame.Rect(150, 550, 250 , 100)#종료 버튼
 
 start = False
-running = True
 while running:
   click_pos = None
 
@@ -21,12 +25,16 @@ while running:
       running = False
     elif event.type == pygame.MOUSEBUTTONUP:
       click_pos = pygame.mouse.get_pos()
-      print(click_pos)
 
   if click_pos != None:
     if start_button.collidepoint(click_pos):
       start = True
-      running = False
+  
+  if start:
+    display_game_screen()
+
+  else:
+    display_start_screen()
 
   
   pygame.display.update()
