b4886d0768af7a7336dcd63f6ff87c5a278e2753
jnhyk <101388279+jnhyk@users.noreply.github.com>
2022-05-11 11:42:01 +0900

    Update 2048_option.py

--- 2048_option.py
+++ 2048_option.py
@@ -8,33 +8,33 @@ pygame.init()
 screen_width = PIXEL * SIZE
 screen_height = PIXEL * SIZE + SCORE_PIXEL
 
-screen = pygame.display.set_mode((screen_width, screen_height))
+screen = pygame.display.set_mode((screen_width, screen_height))#배경
 pygame.display.set_caption("2048 OPTION")
 background = pygame.image.load("C:/Users/hth09/OneDrive/바탕 화면/desktop/2048/background.png")
 screen.blit(background, (0,0))
 
-option_font = pygame.font.Font(None, 100)
+option_font = pygame.font.Font(None, 100)#상단option글씨
 option = option_font.render("OPTION", True, (137, 114, 84))
 screen.blit(option, (PIXEL, SCORE_PIXEL // 2))
 
-bgm_emoji = pygame.image.load("C:/Users/hth09/OneDrive/바탕 화면/desktop/2048/sound.png")
+bgm_emoji = pygame.image.load("C:/Users/hth09/OneDrive/바탕 화면/desktop/2048/sound.png")#왼쪽 배경음 버튼
 bgm_button = pygame.Rect(PIXEL , PIXEL * 2 - 50, PIXEL //3 * 2 , PIXEL // 3 * 2)
 pygame.draw.rect(screen,(137, 114, 84), bgm_button,45,15)
 screen.blit(bgm_emoji, (PIXEL + 10, PIXEL * 2 -40))
 
 
-sound_emoji = pygame.image.load("C:/Users/hth09/OneDrive/바탕 화면/desktop/2048/sound2.png")
+sound_emoji = pygame.image.load("C:/Users/hth09/OneDrive/바탕 화면/desktop/2048/sound2.png")#오른쪽 효과음 버튼
 sound_button = pygame.Rect(PIXEL + PIXEL // 3 * 4, PIXEL * 2 - 50, PIXEL //3 * 2 , PIXEL // 3 * 2)
 pygame.draw.rect(screen,(137, 114, 84), sound_button,45,15)
 screen.blit(sound_emoji, (PIXEL + PIXEL // 3 * 4 + 10, PIXEL * 2 - 40))
 
-score_button = pygame.Rect(PIXEL, PIXEL * (SIZE - 1),PIXEL * 2 , PIXEL // 3 * 2)
+score_button = pygame.Rect(PIXEL, PIXEL * (SIZE - 1),PIXEL * 2 , PIXEL // 3 * 2)#score board 버튼
 pygame.draw.rect(screen,(137, 114, 84), score_button,45,15)
 score_font = pygame.font.Font(None, 50)
 score = score_font.render("SCORE BOARD", True, (0, 0, 0))
 screen.blit(score, (PIXEL + 8 , PIXEL * (SIZE - 1) + PIXEL // 4))
 
-quit_button = pygame.Rect(PIXEL, PIXEL * SIZE, PIXEL * 2 , PIXEL // 3 * 2)
+quit_button = pygame.Rect(PIXEL, PIXEL * SIZE, PIXEL * 2 , PIXEL // 3 * 2)#종료 버튼
 pygame.draw.rect(screen,(137, 114, 84), quit_button,45,15)
 quit_font = pygame.font.Font(None, 50)
 quit = quit_font.render("QUIT", True, (0, 0, 0))
