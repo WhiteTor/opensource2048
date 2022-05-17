e85f5118189aff1787ad96c8ec27e0dc12aa05a5
jnhyk <101388279+jnhyk@users.noreply.github.com>
2022-05-17 18:01:25 +0900

    Update 2048_main.py
    임의의 블럭 2개 초기 생성

--- 2048_main.py
+++ 2048_main.py
@@ -1,57 +1,70 @@
+import random
 import pygame
 
-PIXEL = 135
-SCORE_PIXEL = 185
-SIZE = 4
 
 def display_start_screen():
   pygame.display.set_caption("2048 START")
   global start_button
   background = pygame.image.load("start_screen.jpg")
   screen.blit(background, (0,0))
-  start_button = pygame.Rect(150, 550, 250 , 100)#종료 버튼
+  start_button = pygame.Rect(146, 549, 250 , 100)#종료 버튼
 
 def display_game_screen():
   pygame.display.set_caption("2048")
   global option_button
+  global undo_button
+  global score_button
   background = pygame.image.load("main.jpg")
+  block_c = pygame.image.load("block_c.png")
   screen.blit(background, (0,0))
   option_button = pygame.Rect(460, 130, 50 , 50)
+  undo_button = pygame.Rect(367, 128, 54, 54)
+  score_button = pygame.Rect(355, 74, 53, 29) 
+
+  for i in range(0,4):
+    y = 211 + 118 * i
+    for j in range(0,4):
+      x = 37 + 119 * j
+      block[i].append((x,y))
+    block.append([])
+
+  screen.blit(block_c, block[loc1_x][loc1_y])
+  screen.blit(block_c, block[loc2_x][loc2_y])
+
+  
+
+
+def display_rank_screen():
+  global rank_quit_button
+  pygame.display.set_caption("2048 RANKING")
+  background = pygame.image.load("Ranking.png")
+  screen.blit(background, (0,0))
+  rank_quit_button = pygame.Rect(387, 660, 132, 42)
 
 def display_option_screen():
   pygame.display.set_caption("2048 OPTION")
-  global quit_button
-  background = pygame.image.load("background.png")
+  global option_quit_button
+  global bgm_button
+  global sound_button
+  global restart_button
+  background = pygame.image.load("option.png")
   screen.blit(background, (0,0))
 
-  option_font = pygame.font.Font(None, 100)#상단option글씨
-  option = option_font.render("OPTION", True, (137, 114, 84))
-  screen.blit(option, (PIXEL, SCORE_PIXEL // 2))
-
-  bgm_emoji = pygame.image.load("sound.png")#왼쪽 배경음 버튼
-  bgm_button = pygame.Rect(PIXEL , PIXEL * 2 - 50, PIXEL //3 * 2 , PIXEL // 3 * 2)
-  pygame.draw.rect(screen,(137, 114, 84), bgm_button,45,15)
-  screen.blit(bgm_emoji, (PIXEL + 10, PIXEL * 2 -40))
-
-
-  sound_emoji = pygame.image.load("sound2.png")#오른쪽 효과음 버튼
-  sound_button = pygame.Rect(PIXEL + PIXEL // 3 * 4, PIXEL * 2 - 50, PIXEL //3 * 2 , PIXEL // 3 * 2)
-  pygame.draw.rect(screen,(137, 114, 84), sound_button,45,15)
-  screen.blit(sound_emoji, (PIXEL + PIXEL // 3 * 4 + 10, PIXEL * 2 - 40))
-
-  restart_button = pygame.Rect(PIXEL, PIXEL * (SIZE - 1),PIXEL * 2 , PIXEL // 3 * 2)#restart버튼
-  pygame.draw.rect(screen,(137, 114, 84), restart_button,45,15)
-  restart_font = pygame.font.Font(None, 50)
-  restart = restart_font.render("RESTART", True, (0, 0, 0))
-  screen.blit(restart, (PIXEL + PIXEL // 3 + 10, PIXEL * (SIZE - 1) + PIXEL // 4))
-
-  quit_button = pygame.Rect(PIXEL, PIXEL * SIZE, PIXEL * 2 , PIXEL // 3 * 2)#종료 버튼
-  pygame.draw.rect(screen,(137, 114, 84), quit_button,45,15)
-  quit_font = pygame.font.Font(None, 50)
-  quit = quit_font.render("QUIT", True, (0, 0, 0))
-  screen.blit(quit, (PIXEL + PIXEL // 3 * 2, PIXEL * SIZE + PIXEL // 4))
+  bgm_button = pygame.Rect(130,216,94,94)
+  sound_button = pygame.Rect(316, 410, 94, 94)
+  restart_button = pygame.Rect(130, 404, 280, 93)#restart버튼
+  option_quit_button = pygame.Rect(130, 541, 280, 93)#종료 버튼
 
 pygame.init()
+block = []
+block.append([])
+loc1_x = random.randrange(0,4)
+loc1_y = random.randrange(0,4)
+loc2_x = random.randrange(0,4)
+loc2_y = random.randrange(0,4)
+while loc1_x != loc2_x and loc1_y != loc2_y:
+  loc2_x = random.randrange(0,4)
+  loc2_y = random.randrange(0,4)
 screen_width = 540
 screen_height = 725
 
@@ -60,6 +73,7 @@ screen = pygame.display.set_mode((screen_width, screen_height))#배경
 start = False
 running = True
 option = False
+rank = False
 while running:
   click_pos = None
 
@@ -68,6 +82,7 @@ while running:
       running = False
     elif event.type == pygame.MOUSEBUTTONUP:
       click_pos = pygame.mouse.get_pos()
+      print(click_pos)
 
   if click_pos:
     if start_button.collidepoint(click_pos):
@@ -78,15 +93,25 @@ while running:
       display_option_screen()
 
       if click_pos:
-        if quit_button.collidepoint(click_pos):
-          running = False
+        if option_quit_button.collidepoint(click_pos):
+          option = False
+
+    elif rank:
+        display_rank_screen()
+
+        if click_pos:
+          if rank_quit_button.collidepoint(click_pos):
+            rank = False
 
     else:
       display_game_screen()
+
     
-    if click_pos:
-        if option_button.collidepoint(click_pos):
-          option = True
+      if click_pos:
+          if option_button.collidepoint(click_pos):
+            option = True
+          elif score_button.collidepoint(click_pos):
+            rank = True
 
   else:
     display_start_screen()
